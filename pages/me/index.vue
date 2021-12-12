886b9cce98a1f4eb0c4cb5128d0fde016afd37f3
chrislarocque <larocque.christopher@gmail.com>
2021-12-11 21:06:07 -0500

    About page

--- pages/me/index.vue
+++ pages/me/index.vue
@@ -1,3 +1,48 @@
 <template>
-  <h1>Me</h1>
+  <div class="columns is-centered is-vcentered p-6">
+    <div class="column is-one-third">
+      <div class="content">
+        <h1>About me</h1>
+        <p>
+          I'm a web developer based near Boston, MA with ~7 years experience
+          building content oriented, user-first web applications.
+        </p>
+        <div class="buttons">
+          <a
+            href="mailto:larocque.christopher@gmail.com"
+            class="button is-black"
+            >Email</a
+          >
+          <a href="https://github.com/ChrisLaRocque" class="button is-black"
+            ><Icon name="Github" />&nbsp;<span
+              style="display: block"
+              class="ml-1"
+              >Github</span
+            ></a
+          >
+        </div>
+      </div>
+    </div>
+    <div class="column is-one-third">
+      <figure class="image">
+        <img src="~/assets/img/headshot.jpeg" class="is-rounded" alt="" />
+      </figure>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  head() {
+    return {
+      title: `About | Chris LaRocque`,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: `I'm a web developer based near Boston with experience building React and Vue framework based apps based around marketing orgs and user first design.`,
+        },
+      ],
+    };
+  },
+};
+</script>
